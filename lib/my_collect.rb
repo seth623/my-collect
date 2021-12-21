79a8c3d552ee105e7a4c72309d081c54967988ef
seth <seth623@gmail.com>
2021-12-21 15:00:51 -0700

    Completed Assignment

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+
+    counter = 0
+    collection = []
+
+    while counter < array.length
+        yielded_var = yield(array[counter])
+        collection << yielded_var
+        counter += 1 
+    end
+
+    return collection
+
+end 
+
 
 
